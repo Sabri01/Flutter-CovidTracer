6109d2f51ea56bf65912bed73731b59419577e0c
Sabri01 <Sabri01>
2020-07-13 23:12:19 +0100

    first

---
+++ android/app/src/main/kotlin/com/example/CovidTracerProject/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.CovidTracerProject
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'login.dart';
+import 'nearby_interface.dart';
+import 'registration.dart';
+import 'welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WelcomeScreen(),
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        NearbyInterface.id: (context) => NearbyInterface(),
+      },
+    );
+  }
+}
